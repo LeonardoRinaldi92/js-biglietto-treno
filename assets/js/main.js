6aabdce16be0217bb1f12ed2f7218612d6eec7de
LeonardoRinaldi92 <leonardo.rinaldi.ph@gmail.com>
2023-03-23 10:49:13 +0100

    COMPLETO + sconti spiegati

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,13 +3,20 @@ const form = document.getElementById("form");
 form.addEventListener("submit", function (event) {
     event.preventDefault();
 
+    function calcolaprezzo(distanza, sconto) {
+        return ((distanza * 0.21) * sconto).toFixed(2)
+    }
+
     let distanza = document.getElementById("distanza").value;
-    let sconto = 1;
+    
     if (document.getElementById("eta16").checked){
-        sconto = 0.8;
+        document.getElementById('prezzoFinale').innerText = calcolaprezzo(distanza, 0.8) + " euro";
+        document.getElementById('prezzoSconto').innerText = 'Hai risparmiato' + ' ' + (calcolaprezzo(distanza, 1) - calcolaprezzo(distanza, 0.8)).toFixed(2) + " euro, grazie alla categoria minorenni"    
     } else if (document.getElementById("eta65").checked){
-        sconto = 0.6;
-    } 
-    let prezzo = ((distanza * 0.21) * sconto).toFixed(2)
-    document.getElementById('prezzoFinale').innerText = prezzo + "euro"}
-    );
+        document.getElementById('prezzoFinale').innerText = calcolaprezzo(distanza, 0.6) + " euro";
+        document.getElementById('prezzoSconto').innerText = 'Hai risparmiato' + ' ' + (calcolaprezzo(distanza, 1) - calcolaprezzo(distanza, 0.6)).toFixed(2) + " euro, perchè alla categoria over65"    
+    } else {
+        document.getElementById('prezzoFinale').innerText = calcolaprezzo(distanza, 1) + " euro";
+    }
+    
+});
